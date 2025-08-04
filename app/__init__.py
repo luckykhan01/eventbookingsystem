38e6521a990f231f2fb04f6ece8da295708e83d3
Tursynkhan Baisal <xbaisal@Tursynkhans-MacBook-Air.local>
2025-08-04 21:13:24 +0500

    Initialization with login/logout

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, url_for, redirect
+from config import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
